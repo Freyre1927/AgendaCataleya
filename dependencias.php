1e4b8c3acbc079e976b5aa5a987a34285fbc1ae0
Roche1927 <jfreyre17@gmail.com>
2021-04-25 01:53:48 -0500

    Montaje bootstrap,jquery etc

---
+++ dependencias.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" class="text/css" href="public/bootstrap/bootstrap.min.css" >
+<link rel="stylesheet" class="text/css" href="public/fontawesome/css/all.css" >
+<!--Datatable js -->
+<link rel="stylesheet" class="text/css" href="public/datatable/bootstrap.css" >
+<link rel="stylesheet" class="text/css" href="public/datatable/dataTables.bootstrap4.min.css" >
+<!-- -->
+
+<script src="public/jquery-3.6.0.min.js"></script>
+<script src="public/bootstrap/popper.min.js"></script>
+<script src="public/bootstrap/bootstrap.min.js"></script>
+
+<!--Datatable js -->
+<script src="public/datatable/jquery.dataTables.min.js"></script>
+<script src="public/datatable/dataTables.bootstrap4.min.js"></script>
+<!-- -->
+<script src="public/sweetalert.min.js"></script>
