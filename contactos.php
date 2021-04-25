b3e67ea25d1feb970244e7776559be2c40238bbb
Roche1927 <jfreyre17@gmail.com>
2021-04-25 10:30:01 -0500

    jumbotron

---
+++ contactos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Contactos</title>
+    <link rel="icon" type="image/jpg" href="public/img/favicon.jpg">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    
+    <?php require_once "dependencias.php"; ?>
+</head>
+<body>
+    <div class="container">
+        <?php require_once "menu.php"; ?>
+
+        <div class="jumbotron">
+            <h1 class="display-4">Contactos</h1>
+            <button class="btn btn-primary">
+               <span class="fas fa-plus-circle"></span> Agregar contacto
+            </button>
+            <hr class="my-4">
+        </div>
+    </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,15 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>Contactos Cataleya</title>
-    <link rel="icon" type="image/jpg" href="public/img/favicon.jpg">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    
-    <?php require_once "dependencias.php"; ?>
-</head>
-<body>
-    
-</body>
+    <head>
+        <title>Contactos Cataleya</title>
+        <link rel="icon" type="image/jpg" href="public/img/favicon.jpg">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+        <?php require_once "dependencias.php"; ?>
+    </head>
+    <body>
+        <div class="container">
+            <?php require_once "menu.php"; ?>
+            <div class="jumbotron">
+                <h1 class="display-4">Agenda Cataleya</h1>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <img src="public/img/cataleya.jpg" class="img-fluid" alt="Responsive image">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <table class="table table-hover">
+                                    <tr>
+                                        <th>
+                                            Agenda 2021
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th>
+                                            By Juan C. Freyre
+                                        </th>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <hr class="my-4">
+                <p>
+                    Agenda de contactos con php y mysql<br>
+                    By Juan C. Freyre.
+                </p>
+
+            </div>
+        </div>
+
+    </body>
 </html>
